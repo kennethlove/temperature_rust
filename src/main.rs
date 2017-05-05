23f7124498c11e528c3c67a19c44233237eb90d9
Kenneth Love <kenneth@gigantuan.net>
2017-05-05 10:34:53 -0700

    works

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ extern crate serde;
 extern crate serde_json;
 extern crate url;
 
+use std::io;
 use std::io::Read;
 use serde_json::Value;
 use url::Url;
@@ -15,7 +16,7 @@ fn get_json(zip: &str) -> Value {
     let key = "1aa84f76ad87af18";
     let url_str = format!("http://api.wunderground.com/api/{}/conditions/q/{}.json", key, zip);
     let url = Url::parse(url_str.as_str()).unwrap();
-    let mut resp = reqwest::get(Url::parse(url.as_str()).unwrap()).unwrap();
+    let mut resp = reqwest::get(url).unwrap();
     assert!(resp.status().is_success());
 
     let mut content = String::new();
@@ -25,6 +26,19 @@ fn get_json(zip: &str) -> Value {
 }
 
 fn main() {
-    let json = get_json("97068");
-    println!("{:?}", json);
+    println!("What area would you like a forecast for? ");
+    let mut zip = String::new();
+
+    io::stdin()
+        .read_line(&mut zip)
+        .expect("Failed to read line");
+    let zip = zip.trim();
+
+    let json = get_json(zip);
+    let chars_to_trim: &[char] = &['"'];
+    let ref feels_like = json["current_observation"]["feelslike_string"].to_string();
+    let feels_like = feels_like.trim_matches(chars_to_trim);
+    let ref weather = json["current_observation"]["weather"].to_string();
+    let weather = weather.trim_matches(chars_to_trim);
+    println!("Weather for {} is currently {}, and temp feels like {}", zip, weather, feels_like);
 }
